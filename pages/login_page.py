85f3a7fe99f3a553b0a8f11d1c6685f45d8e6caa
xmb1pb <mzahravin@gmail.com>
2022-02-03 19:05:11 +0700

    added locators.py, added login_page.py

---
+++ pages/login_page.py
@@ -0,0 +1,20 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        assert 'login' in self.browser.current_url, 'Seems that URL is not login URL'
+
+    def should_be_login_form(self):
+        assert self.is_element_present(
+            *LoginPageLocators.LOGIN_FORM), 'Seems that login form is missing from login page'
+
+    def should_be_register_form(self):
+        assert self.is_element_present(
+            *LoginPageLocators.REGISTER_FORM), 'Seems that registration form is missing from login page'
