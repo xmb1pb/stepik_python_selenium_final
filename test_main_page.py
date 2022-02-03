be14b41c1c4f4c433400b10ba60292305491700b
xmb1pb <mzahravin@gmail.com>
2022-02-03 17:24:37 +0700

    POM implementation

--- test_main_page.py
+++ test_main_page.py
@@ -1,11 +1,14 @@
 import pytest
+import time
+from .pages.main_page import MainPage
 
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+    page = MainPage(browser, link)
+    page.open()
+    time.sleep(10)
+    page.go_to_login_page()
 
 
 if __name__ == '__main__':
