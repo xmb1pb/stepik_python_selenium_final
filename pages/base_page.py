7c6bd9475a7978a583dc3da020b53e1dd343967b
xmb1pb <mzahravin@gmail.com>
2022-02-03 18:34:56 +0700

    added is_element_present method

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,7 +1,17 @@
+from selenium.common.exceptions import NoSuchElementException
+
 class BasePage:
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
 
     def open(self):
         self.browser.get(self.url)
+
+    def is_element_present(self, by_method, elem_locator):
+        try:
+            self.browser.find_element(by_method, elem_locator)
+        except NoSuchElementException:
+            return False
+        return True
--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,4 @@
 import pytest
-import time
 from .pages.main_page import MainPage
 
 
@@ -7,8 +6,8 @@ def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
-    time.sleep(10)
     page.go_to_login_page()
+    page.should_be_login_link()
 
 
 if __name__ == '__main__':
